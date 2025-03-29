9d85aef65485d60d7e7123daf170214c37075038
alissonyanza1234 <alissonyanza1234@gmail.com>
2025-03-29 21:08:09 +0000

    seven

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'wpr-water-quality-dashboard',
+  webDir: 'dist/wpr-water-quality-dashboard/browser'
+};
+
+export default config;
